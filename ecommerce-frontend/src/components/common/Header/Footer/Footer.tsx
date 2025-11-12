d7689966c30903b81b55931486d6a75bb4352616
Mohamed Ali <mohamed.ali.mahmoud.ali@gmail.com>
2025-11-12 10:38:47 +0200

    change footer design to include hr and text below it

--- ecommerce-frontend/src/components/common/Header/Footer/Footer.tsx
+++ ecommerce-frontend/src/components/common/Header/Footer/Footer.tsx
@@ -1,9 +1,8 @@
 const Footer = () => {
   return (
-    <footer className="container mx-auto flex justify-center">
-      <span className="bg-cyan-400 p-2 rounded-lg font-bold text-white">
-        © 2025 Ecom All Right Reserved
-      </span>
+    <footer className="container mx-auto flex flex-col justify-center items-center">
+      <hr className="w-full border-black border-t-2" />
+      <span className="font-bold mt-2">© 2025 Ecom All Right Reserved</span>
     </footer>
   );
 };
